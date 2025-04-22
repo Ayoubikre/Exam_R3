01927f68d47032cf9b85d622e2954dbcff18bbbc
Ayoubikre <ayoubikrr@gmail.com>
2025-04-22 01:55:37 +0000

    add

--- LEVEL-1/filter/filter.c
+++ LEVEL-1/filter/filter.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   filter.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
+/*   By: noctis <noctis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:05:40 by mlaffita          #+#    #+#             */
-/*   Updated: 2025/04/21 21:49:11 by aakritah         ###   ########.fr       */
+/*   Updated: 2025/04/21 22:56:47 by noctis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,78 +14,101 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
-int ft_strlen(char *t)
+int	ft_strlen(char *t)
 {
-    int i=0;
-    while(t[i])
-        i++;
-    return i;
+	int	i;
+
+	i = 0;
+	while (t[i])
+		i++;
+	return (i);
 }
 
-char *ft_strdup(char *t)
+char	*ft_strdup(char *t)
 {
-    int i=0;
-    int s=ft_strlen(t);
+	int		i;
+	int		s;
+	char	*str;
 
-    char *str=malloc(s+1);
-    if(!str)
-        return NULL;
-    while(t[i])
-    {
-        str[i]=t[i];
-        i++;
-    }    
-    str[i]='\0';
-    return str;
+	i = 0;
+	s = ft_strlen(t);
+	str = malloc(s + 1);
+	if (!str)
+		return (NULL);
+	while (t[i])
+	{
+		str[i] = t[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
-int ft_check(char *t1, char *t2)
+int	ft_check(char *t1, char *t2)
 {
-    int s1=ft_strlen(t1);
-    int s2=ft_strlen(t2);
-    int i=0;
-    if(s1<s2)
-        return 0;
-    while(t2[i] && t2[i]==t1[i])
-        i++;
-    if(i!=s2)
-        return 0;
-    return 1;
+	int	s1;
+	int	s2;
+	int	i;
+
+	s1 = ft_strlen(t1);
+	s2 = ft_strlen(t2);
+	i = 0;
+	if (s1 < s2)
+		return (0);
+	while (t2[i] && t2[i] == t1[i])
+		i++;
+	if (i != s2)
+		return (0);
+	return (1);
 }
 
-int main(int ac, char **ar)
+void	ft_error(void)
 {
-    char t1[100000];
-    
-    if(ac<2)
-        return 1;
-    else
-    {
-        int i=0;
-        int n=0;
-        int j=0;
-        int s=ft_strlen(ar[1]);
-        
-        n = read(0, t1, 100000);
-        if(n<=0)
-            return 1;
-        while(t1[i])
-        {
-            if(ft_check(t1+ i , ar[1]) == 1)
-            {
-                j=0;
-                while(j<s)
-                {
-                    t1[i+j]= '*';
-                    j++;
-                }
-            }
-            i++;
-        }    
-    }
-    int s2=ft_strlen(t1);
-    write(1, t1, s2);
-    // write(1, "\n", 1);
-    return(0);
+	write(2, "ERROR:", 6);
+	perror("try again");
+}
+
+int	main(int ac, char **ar)
+{
+	char	t1[100000];
+	int		i;
+	int		n;
+	int		j;
+	int		s;
+	int		s2;
+
+	if (ac != 2)
+	{
+		ft_error();
+		return (1);
+	}
+	else
+	{
+		i = 0;
+		n = 0;
+		j = 0;
+		s = ft_strlen(ar[1]);
+		n = read(0, t1, 100000);
+		if (n <= 0)
+		{
+			ft_error();
+			return (1);
+		}
+		while (t1[i])
+		{
+			if (ft_check(t1 + i, ar[1]) == 1)
+			{
+				j = 0;
+				while (j < s)
+				{
+					t1[i + j] = '*';
+					j++;
+				}
+			}
+			i++;
+		}
+	}
+	s2 = ft_strlen(t1);
+	write(1, t1, s2);
+	return (0);
 }
--- LEVEL-1/scanf/scanf.c
+++ LEVEL-1/scanf/scanf.c
@@ -3,10 +3,157 @@
 /*                                                        :::      ::::::::   */
 /*   scanf.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlaffita <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: noctis <noctis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 13:06:06 by mlaffita          #+#    #+#             */
-/*   Updated: 2025/04/01 13:06:07 by mlaffita         ###   ########.fr       */
+/*   Created: 2025/04/21 22:59:11 by noctis            #+#    #+#             */
+/*   Updated: 2025/04/22 01:55:11 by noctis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_str(va_list ptr)
+{
+	int		i;
+	char	n;
+	char	*t;
+
+	i = 0;
+	t = va_arg(ptr, char *);
+	while ((n = fgetc(stdin)) != EOF)
+	{
+		if (isspace(n))
+			break ;
+		t[i] = n;
+		i++;
+	}
+	t[i] = '\0';
+	if (i == 0)
+		return (EOF);
+	return (1);
+}
+
+int	ft_char(va_list ptr, int f)
+{
+	char	*c;
+	int		i;
+
+	c = va_arg(ptr, char *);
+	i = 0;
+	if (f == 1)
+	{
+		while ((*c = fgetc(stdin)) != EOF && isspace(*c))
+			;
+	}
+	else
+		*c = fgetc(stdin);
+	if (*c == EOF)
+		return (EOF);
+	return (1);
+}
+
+int	ft_num(va_list ptr)
+{
+	char	c;
+	long	i;
+	long	*n;
+	long	f;
+
+	i = 0;
+	f = 1;
+	n = va_arg(ptr, long *);
+	while ((c = fgetc(stdin)) != EOF && isspace(c))
+		;
+	if (c == '-' || c == '+')
+	{
+		if (c == '-')
+		{
+			f *= -1;
+		}
+	}
+	else
+		ungetc(c, stdin);
+	while ((c = fgetc(stdin)) != EOF && isdigit(c))
+	{
+		i = (i * 10) + (c - '0');
+	}
+	*n = i * f;
+	if (c == EOF && i == 0)
+		return (EOF);
+	return (1);
+}
+
+int	ft_scanf(const char *t, ...)
+{
+	int		i;
+	int		count;
+	int		n;
+	va_list	ptr;
+
+	i = 0;
+	count = 0;
+	va_start(ptr, t);
+	if (!t)
+		return (-1);
+	while (t[i])
+	{
+		if (t[i] == '%' && t[i + 1] != '\0')
+		{
+			if (t[i + 1] == 's')
+			{
+				n = ft_str(ptr);
+				if (n != EOF)
+					count++;
+				else
+					break ;
+			}
+			else if (t[i + 1] == 'd')
+			{
+				n = ft_num(ptr);
+				if (n != EOF)
+					count++;
+				else
+					break ;
+			}
+			else if (t[i + 1] == 'c')
+			{
+				if (i == 0 || (i != 0 && t[i - 1] != ' '))
+				{
+					n = ft_char(ptr, 0);
+					if (n != EOF)
+						count++;
+					else
+						break ;
+				}
+				else
+				{
+					n = ft_char(ptr, 1);
+					if (n != EOF)
+						count++;
+					else
+						break ;
+				}
+			}
+		}
+		i++;
+	}
+	va_end(ptr);
+	return (count);
+}
+
+int	main(void)
+{
+	long	num;
+	char	t[100];
+	int		n;
+
+	// Testing %d input
+	printf("Enter a number: ");
+	n = ft_scanf("%d%s", &num, t);
+	printf("You entered: %ld-%s-%d", num, t, n);
+	return (0);
+}
